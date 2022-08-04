9de92c45acd2a9480f2346b6f63021d0c3728031
Kashu_happy <knashipa@gmail.com>
2022-08-04 21:26:07 +0000

    Slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -8,6 +8,7 @@ const Container = styled.div`
     display: flex;
     padding: 20px;
     background-color: magenta;
+    position: relative;
 `
 const Arrow = styled.div`
     width: 50px;
@@ -17,15 +18,46 @@ const Arrow = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    left: ${props => props.direction === "left" && "10px"};
+    right: ${props => props.direction === "right" && "10px"};
+    cursor: pointer;
+    opacity: 0.6;
+`
+const Wrapper = styled.div`
+    height: 100%;
+`
+const Slide = styled.div`
+    display: flex;
+    align-items: center;
+`
+const ImgContainer = styled.div`
+    flex: 1;
+`
+const Image = styled.img`
+
+`
+
+const InfoContainer = styled.div`
+    flex: 1;
 `
 
 const Slider = () => {
   return (
     <Container>
-        <Arrow>
+        <Arrow direction = "left">
             <ArrowLeftOutlined/>
         </Arrow>
-        <Arrow>
+        <Wrapper>
+            <ImgContainer>
+            <Image src=""/>
+            </ImgContainer>
+            <InfoContainer></InfoContainer>
+        </Wrapper>
+        <Arrow direction = "right">
             <ArrowRightOutlined/>
         </Arrow>
     </Container>
