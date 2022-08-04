ac748e2471571afc2676ca2f0d1f7942b20ef835
Kashu_happy <knashipa@gmail.com>
2022-08-04 15:55:58 +0000

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,72 @@
+import { Badge } from '@material-ui/core'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
 import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
     height: 50px;
-    background-color: rgb(52, 212, 212);
-    display: flex;
+`
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display:flex;
+    align-items: center;
     justify-content: space-between;
-`;
+`
+const Right = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+const SearchContainer = styled.div`
+    border: 1px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
+const Input = styled.input`
+    border: none;
+`
+const MenuItem = styled.div`
+    font-size: 14;
+    cursor: pointer;
+`
+
+const Left = styled.div`
+    flex: 1;
+`
+const Logo = styled.h1`
+    font-weight: bold;
+    margin-left: 50px;
+`
 
 const Navbar = () => {
   return (
     <Container>
-        <div>Left</div>
-        <div>Center</div>
-        <div>Right</div>
+      <Wrapper>
+      <Left>
+        <Logo>HNK</Logo>
+      </Left>
+      <Right>
+        <Language>EN</Language>
+        <SearchContainer>
+          <Input/>
+          <Search/>
+        </SearchContainer>
+        <MenuItem>Register</MenuItem>
+        <MenuItem>Sign up</MenuItem>
+        <MenuItem>
+        <Badge badgeContent={4} color="primary">
+          <ShoppingCartOutlined />
+        </Badge>
+        </MenuItem>
+      </Right>
+      </Wrapper>
     </Container>
   )
 }
