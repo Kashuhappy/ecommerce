ecf264382d2189693815865be45161eaf9a50631
Kashu_happy <knashipa@gmail.com>
2022-08-04 20:45:55 +0000

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,8 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-    height: 60px;
+    height: 40px;
+    padding-bottom: 35px;
 `
 
 const Wrapper = styled.div`
