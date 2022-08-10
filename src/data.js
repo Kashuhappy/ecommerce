ca7f0f9b05ff8464e0e487e3e47de36b29c568f0
Kashu_happy <knashipa@gmail.com>
2022-08-10 14:26:29 +0000

    Updates on data file

--- src/data.js
+++ src/data.js
@@ -1,19 +1,22 @@
 export const sliderItems = [
     {
         id: 1,
-        img: "./images/image.png",
+        img: "https://i.ibb.co/RBZ6nwW/shoe3.png",
+        alt: "Air Jordan 1",
         title: "SUMMER COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
     },
     {
         id: 2,
-        img: "./images/image.png",
+        img: "https://i.ibb.co/F5F9vcf/shoe.png", 
+        alt: "White boots",
         title: "AUTUMN COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS", 
     },
     {
         id: 3,
-        img: "./images/image.png",
+        img: "https://i.ibb.co/mN5Fvm7/shoe2.png",
+        alt: "Air force 1",
         title: "POPULAR COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS", 
     },
