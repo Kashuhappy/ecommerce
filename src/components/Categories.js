7732e367ff77b6e262c531e9d7fd82b30504e635
Kashu_happy <knashipa@gmail.com>
2022-08-12 22:11:05 +0000

    Adding react

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,7 +4,8 @@ import { categories } from "../data"
 import CategoryItem from "./CategoryItem"
 
 const Container = styled.div`
-
+    display: flex;
+    padding: 20px;
 `
 
 const Categories = () => {
