7ef2cba5635ba3fcde8dfffb268735b230305df2
Kashu_happy <knashipa@gmail.com>
2022-08-12 21:37:29 +0000

    Categories added

--- src/data.js
+++ src/data.js
@@ -21,3 +21,21 @@ export const sliderItems = [
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS", 
     },
 ]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/RBZ6nwW/shoe3.png",
+        title: "SUMMER SALE",
+    },
+    {
+        id: 1,
+        img: "https://i.ibb.co/RBZ6nwW/shoe3.png",
+        title: "SUMMER SALE",
+    },
+    {
+        id: 1,
+        img: "https://i.ibb.co/RBZ6nwW/shoe3.png",
+        title: "SUMMER SALE",
+    }
+]
