a1209c84c33bf5c2c2ee05c6bc202ac3d9abcb4d
Kashu_happy <knashipa@gmail.com>
2022-08-13 18:36:30 +0000

    Category items

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -6,13 +6,15 @@ import CategoryItem from "./CategoryItem"
 const Container = styled.div`
     display: flex;
     padding: 20px;
+    justify-content: space-between;
+    background-color: #171941;
 `
 
 const Categories = () => {
   return (
     <Container>
         {categories.map(item => (
-            <CategoryItem item={item}/>
+            <CategoryItem item={item} key={item.id} />
         ))}
     </Container>
   )
