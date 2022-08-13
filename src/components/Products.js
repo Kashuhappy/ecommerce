9b0f131b75d31790864849bd2f0ed300dc8198f4
Kashu_happy <knashipa@gmail.com>
2022-08-13 19:30:58 +0000

    Products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import { popularProducts } from '../data'
+import Product from './Product'
 
 const Container = styled.div`
-
+    padding: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    background-color: #171941;
 `
 
 const Products = () => {
   return (
     <Container>
-        Products
+        {popularProducts.map(item => (
+            <Product item={item} key={item.id} />
+        ))}
     </Container>
   )
 }
