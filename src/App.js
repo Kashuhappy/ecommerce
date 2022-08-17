ac0cdf822c9b39c61c1d71d66da24f336f664d4b
Kashu_happy <knashipa@gmail.com>
2022-08-17 01:00:53 +0000

    Checking out cart page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Cart from './pages/Cart';
+//import Login from './pages/Login';
 //import ProductList from './pages/ProductList';
 //import Home from "./pages/Home";
-import Product from './pages/Product';
+//import Product from './pages/Product';
+//import Register from './pages/Register';
 
 function App() {
   return ( 
     <div>
-      <Product/>
+      <Cart/>
     </div>
   );
 }
