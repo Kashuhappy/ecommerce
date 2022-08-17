5261c9f7afcf113fdb119c017468b9c134c8d7fe
Kashu_happy <knashipa@gmail.com>
2022-08-17 00:00:00 +0000

    Registration page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,12 +2,64 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-
+  width: 100vw; // selecting the whole page as the container
+  height: 100vh; // "  "
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #171941;
+`
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 40%;
+  background-color: white;
+`
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`
+const Agreement = styled.span`
+  font-style: 12px;
+  margin: 20px 0px;
+`
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
 `
 
 const Register = () => {
   return (
-    <Container>Register</Container>
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="First Name"/>
+          <Input placeholder="Last Name"/>
+          <Input placeholder="Username"/>
+          <Input placeholder="Email"/>
+          <Input placeholder="Password"/>
+          <Input placeholder="Confirm Password"/>
+          <Agreement>
+            By creating an account, I consent to the processing of my personal data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
   )
 }
 
