b05d7b6578b2c012091edbfd9d4beff20739a46d
Kashu_happy <knashipa@gmail.com>
2022-08-18 18:21:54 +0000

    App page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 //import Cart from './pages/Cart';
 //import Login from './pages/Login';
-import ProductList from './pages/ProductList';
-//import Home from "./pages/Home";
+//import ProductList from './pages/ProductList';
+import Home from "./pages/Home";
 //import Product from './pages/Product';
 //import Register from './pages/Register';
 
 function App() {
   return ( 
     <div>
-      <ProductList/>
+      <Home/>
     </div>
   );
 }
